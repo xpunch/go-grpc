f901932d064b6da2717810132763e4d0aa9f3c24
Asim Aslam <asim@chuhnk.me>
2017-06-12 15:13:41 +0100

    Add function support

--- go-grpc.go
+++ go-grpc.go
@@ -1,6 +1,8 @@
 package grpc
 
 import (
+	"time"
+
 	"github.com/micro/go-micro"
 
 	client "github.com/micro/go-plugins/client/grpc"
@@ -25,4 +27,26 @@ func NewService(opts ...micro.Option) micro.Service {
 
 	// generate and return a service
 	return micro.NewService(options...)
+}
+
+// NewFunction returns a grpc service compatible with go-micro.Function
+func NewFunction(opts ...micro.Option) micro.Function {
+	// our grpc client
+	c := client.NewClient()
+	// our grpc server
+	s := server.NewServer()
+
+	// create options with priority for our opts
+	options := []micro.Option{
+		micro.Client(c),
+		micro.Server(s),
+		micro.RegisterTTL(time.Minute),
+		micro.RegisterInterval(time.Second * 30),
+	}
+
+	// append passed in opts
+	options = append(options, opts...)
+
+	// generate and return a function
+	return micro.NewFunction(options...)
 }
--- go-grpc_test.go
+++ go-grpc_test.go
@@ -1,8 +1,8 @@
 package grpc
 
 import (
+	"sync"
 	"testing"
-	"time"
 
 	"github.com/micro/go-micro"
 	"github.com/micro/go-micro/registry/mock"
@@ -19,7 +19,13 @@ func (t *testHandler) Hello(ctx context.Context, req *hello.Request, rsp *hello.
 	return nil
 }
 
-func TestGRPC(t *testing.T) {
+func TestGRPCService(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	// create mock registry
 	r := mock.NewRegistry()
 
@@ -27,6 +33,11 @@ func TestGRPC(t *testing.T) {
 	service := NewService(
 		micro.Name("test.service"),
 		micro.Registry(r),
+		micro.AfterStart(func() error {
+			wg.Done()
+			return nil
+		}),
+		micro.Context(ctx),
 	)
 
 	// register test handler
@@ -39,11 +50,55 @@ func TestGRPC(t *testing.T) {
 		}
 	}()
 
-	// artificial delay?
-	time.Sleep(time.Millisecond * 10)
+	// wait for start
+	wg.Wait()
 
 	// create client
 	say := hello.NewSayClient("test.service", service.Client())
+
+	// call service
+	rsp, err := say.Hello(context.Background(), &hello.Request{
+		Name: "John",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// check message
+	if rsp.Msg != "Hello John" {
+		t.Fatal("unexpected response %s", rsp.Msg)
+	}
+}
+
+func TestGRPCFunction(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// create service
+	fn := NewFunction(
+		micro.Name("test.function"),
+		micro.Registry(mock.NewRegistry()),
+		micro.AfterStart(func() error {
+			wg.Done()
+			return nil
+		}),
+		micro.Context(ctx),
+	)
+
+	// register test handler
+	hello.RegisterSayHandler(fn.Server(), &testHandler{})
+
+	// run service
+	go fn.Run()
+
+	// wait for start
+	wg.Wait()
+
+	// create client
+	say := hello.NewSayClient("test.function", fn.Client())
 
 	// call service
 	rsp, err := say.Hello(context.Background(), &hello.Request{

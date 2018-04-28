5b86556fa44041d73b82033362e32ff9456e7c4b
Asim Aslam <asim@chuhnk.me>
2018-04-28 10:14:01 +0100

    remove tls

--- examples/greeter/server/main.go
+++ examples/greeter/server/main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"context"
-	"crypto/tls"
 	"log"
-	"time"
 
+	"github.com/micro/go-micro"
 	"github.com/micro/go-grpc"
 	hello "github.com/micro/go-grpc/examples/greeter/server/proto/hello"
-	"github.com/micro/go-micro"
-	mtls "github.com/micro/util/go/lib/tls"
 )
 
 type Say struct{}
@@ -21,18 +18,8 @@ func (s *Say) Hello(ctx context.Context, req *hello.Request, rsp *hello.Response
 }
 
 func main() {
-	cert, _ := mtls.Certificate("127.0.0.1:9090")
-	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{
-			cert,
-		},
-	}
-
 	service := grpc.NewService(
 		micro.Name("go.micro.srv.greeter"),
-		micro.RegisterTTL(time.Second*30),
-		micro.RegisterInterval(time.Second*10),
-		grpc.WithTLS(tlsConfig),
 	)
 
 	// optionally setup command line usage

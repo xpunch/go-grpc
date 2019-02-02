7b68112671727244e4563d9668952e29aebd609b
Asim Aslam <asim@aslam.me>
2019-02-02 20:03:15 +0000

    Make grpc broker default

--- cmd/micro/main.go
+++ cmd/micro/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"os"
 
+	_ "github.com/micro/go-plugins/broker/grpc"
 	_ "github.com/micro/go-plugins/client/grpc"
 	_ "github.com/micro/go-plugins/server/grpc"
 	"github.com/micro/micro/cmd"
 )
 
 func init() {
+	os.Setenv("MICRO_BROKER", "grpc")
 	os.Setenv("MICRO_CLIENT", "grpc")
 	os.Setenv("MICRO_SERVER", "grpc")
 }
